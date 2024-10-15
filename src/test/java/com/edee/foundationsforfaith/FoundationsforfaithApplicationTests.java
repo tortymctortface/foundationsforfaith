26eac9495bd7037ecb648a5d7369602875f16b02
Matt_Duffy <48247103+tortymctortface@users.noreply.github.com>
2024-10-15 22:44:52 +0100

    Initial commit

---
+++ src/test/java/com/edee/foundationsforfaith/FoundationsforfaithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edee.foundationsforfaith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoundationsforfaithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
